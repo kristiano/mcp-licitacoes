770ea212a91f9cf47c9b30fae8c620eb905148ea
Kristiano <kristianob.f@gmail.com>
2026-03-31 19:51:41 -0300

    Delete src/mcp_brasil/data/tce_sp directory

--- src/mcp_brasil/data/tce_sp/constants.py
+++
@@ -1,8 +0,0 @@
-"""Constants for the TCE-SP feature."""
-
-API_BASE = "https://transparencia.tce.sp.gov.br/api"
-
-# JSON endpoints
-MUNICIPIOS_URL = f"{API_BASE}/json/municipios"
-DESPESAS_URL = f"{API_BASE}/json/despesas"  # /{municipio}/{exercicio}/{mes}
-RECEITAS_URL = f"{API_BASE}/json/receitas"  # /{municipio}/{exercicio}/{mes}
